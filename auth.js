71e579b84fcbb18e53362da10c4ff5d5fa438154
sathya9897 <sathyarox7@gmail.com>
2020-02-22 16:31:21 +0530

    added changes

--- auth.js
+++ auth.js
@@ -3,13 +3,14 @@ const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 const secrets = require("./secrets");
 
-let transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: secrets.email,
     pass: secrets.pass
   }
 });
+
 function CreateToken(payload) {
   jwt.sign(payload, "secret", { expiresIn: 2500000 }, function(err, token) {
     if (err) {
@@ -21,23 +22,26 @@ function CreateToken(payload) {
   });
 }
 
-const userData = {
-  id: 25463571,
-  email: "sathyarox7@gmail.com"
-};
-
 router.get("/logout", (req, res) => {
   res.json({});
 });
 router.post("/signin/email", (req, res) => {
-  console.log(req);
-  return CreateToken(userData);
+  const userdata = {
+    id: 5421578,
+    email: req.body.email
+  };
+  return res.json({ token: CreateToken(userdata) });
 });
 router.post("/signup/email", (req, res) => {
   console.log(req.body);
+  const userdata = {
+    id: 5421578,
+    email: req.body.email,
+    newuser: true
+  };
   let mailOptions = {
     from: "sathyarox7@gmail.com",
-    to: "ksathyareddy7@gmail.com",
+    to: req.body.email,
     subject: "Verify Your Email",
     text: "The OTP to verify your account is 451245"
   };
@@ -47,7 +51,7 @@ router.post("/signup/email", (req, res) => {
       res.status(500).json({ error: "something went wrong" });
     } else {
       console.log("EMAIL WAS SENT");
-      return res.json({ token: CreateToken(userData) });
+      return res.json({ token: CreateToken(userdata) });
     }
   });
 });
