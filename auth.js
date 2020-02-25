cdab63f5e7f6df9dfb4018f20b5bff0f457d5196
sathya reddy <ksathyareddy7@gmail.com>
2020-02-25 06:17:09 +0530

    changes

--- auth.js
+++ auth.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
+const validator = require("validator");
 const secrets = require("./secrets");
 
 const transporter = nodemailer.createTransport({
@@ -17,11 +18,22 @@ router.post("/logout", (req, res) => {
 router.post("/signin/email", (req, res) => {
   const userdata = {
     id: 5421578,
-    email: req.body.email
+    email: req.body.email,
+    verified: false
   };
-  jwt.sign(userdata, "secret", { expiresIn: 2500000 }, function(err, token) {
+  let errors = {};
+  if (!validator.isEmail(req.body.email)) {
+    errors["email"] = "invalid email";
+  }
+  if (!validator.isLength(req.body.password, { min: 8 })) {
+    errors["password"] = "password should be atleast 8 characters";
+  }
+  if (Object.keys(errors).length > 0) {
+    return res.status(402).json(errors);
+  }
+  jwt.sign(userdata, "secret", { expiresIn: 60 }, function(err, token) {
     if (err) {
-      return res.status(500).json({ error: "something went wrong" });
+      return res.status(500).json({ server: "something went wrong" });
     }
     return res.json({ token });
   });
@@ -30,11 +42,25 @@ router.post("/signup/email", (req, res) => {
   const userdata = {
     email: req.body.email,
     authType: "manual",
-    detailsForm: true
+    verified: false,
+    detailsUpdated: false
   };
-  jwt.sign(userdata, "secret", { expiresIn: 2500000 }, function(err, token) {
+  let errors = {};
+  if (!validator.isEmail(req.body.email)) {
+    errors["email"] = "invalid email";
+  }
+  if (!validator.isLength(req.body.password, { min: 8 })) {
+    errors["password"] = "password should be atleast 8 characters";
+  }
+  if (req.body.password !== req.body.confirmPassword) {
+    errors["confirm"] = "passwords didn't match";
+  }
+  if (Object.keys(errors).length > 0) {
+    return res.status(402).json(errors);
+  }
+  jwt.sign(userdata, "secret", { expiresIn: 60 }, function(err, token) {
     if (err) {
-      return res.status(500).json({ error: "something went wrong" });
+      return res.status(500).json({ server: "something went wrong" });
     }
     return res.json({ token });
   });
@@ -54,17 +80,30 @@ router.post("/signup/google", (req, res) => {
   return res.status(401).json({ error: "Error authenticating" });
 });
 
-router.post("/verify", (req, res) => {
-  const vercode = 123456;
+router.post("/verification", (req, res) => {
+  const vercode = "123456";
+  console.log(req.body);
   if (req.body.code === vercode) {
-    return res.json({ verified: true });
+    const userdata = {
+      email: req.body.email,
+      authType: "manual",
+      verified: true,
+      detailsUpdated: false
+    };
+    jwt.sign(userdata, "secret", { expiresIn: 2500000 }, function(err, token) {
+      if (err) {
+        return res.status(500).json({ error: "something went wrong" });
+      }
+      return res.json({ token });
+    });
   } else {
     return res.status(401).json({ error: "incorret code" });
   }
 });
 
 router.post("/details", (req, res) => {
-  if (req.body.keys().length >= 9) {
+  console.log(req.body);
+  if (req.body) {
     return res.json({ success: true });
   } else {
     return res.status(401).json({ error: "all fields are mandatory" });
